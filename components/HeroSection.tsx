31c10e98c3d9a3f76cb1e3bd460518281600d73d
Debbie Otuagomah <40691059+thatgirldorian@users.noreply.github.com>
2023-07-06 11:21:29 +0400

    added custom styles + hero component

---
+++ components/HeroSection.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Easy motorcycle booking and rental - right at your fingertips.
+        </h1>
+
+        <p className="hero__subtitle">
+          Elevate your bike rental experience with our smooth and easy booking.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
